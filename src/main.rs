c0d5740092debbf87d7b8668d87c5daf15f5d89f
Tiago Curi <shopper.tiago@gmail.com>
2022-11-26 13:09:20 -0300

    first remote commit

--- src/main.rs
+++ src/main.rs
@@ -160,10 +160,6 @@ impl Spiderphonic {
         }
     }
 
-    fn switch_mode(&mut self, new_mode: Mode) {
-        self.mode = new_mode;
-    }
-
     fn get_initial_checkpoint(&self) -> u64 {
         self.initial_checkpoint.elapsed().unwrap().as_secs()
     }
@@ -177,8 +173,8 @@ impl Spiderphonic {
         let arithmetic_average = self.time_line.calculate_arithmetic_average(elapsed_time); // make conditionally after 90 seconds
 
         let (average_target, _) = self.mode.custom_unwrap();
-        let minimum_time_to_switch = 10;
-        if (elapsed_time >= minimum_time_to_switch) && (arithmetic_average >= average_target) { // what about decreasing effect? 90 seconds is the right time
+        let min_to_switch = 10;
+        if (elapsed_time >= min_to_switch) && (arithmetic_average >= average_target) { // what about decreasing effect? 90 seconds is the right time
             let result = self.mode.get_one_after(self.audio_manager.create_sink());          
             if result.is_ok() {
                 println!("\nincreasing mode...{}", arithmetic_average);
@@ -279,4 +275,10 @@ fn main() {
     });
 
     inputbot::handle_input_events();
+}
+
+
+struct Sample {
+    _path: String,
+    _phase: String,
 }
